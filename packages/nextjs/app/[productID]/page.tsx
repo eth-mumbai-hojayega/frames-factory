42ac2b3a45192dc10f34f021e990cac39105f34b
Bhavya Gor <bhavya.gor9999@gmail.com>
2024-03-31 06:44:20 +0530

    added copy link for warpcast

--- packages/nextjs/app/[productID]/page.tsx
+++ packages/nextjs/app/[productID]/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import React from "react";
+import React, { useMemo } from "react";
 import { NextPage } from "next";
 import NodeModal from "~~/components/NodeModal";
 import ReactFlow from "~~/components/reactflow";
 import { useJourneyForProduct } from "~~/providers/ReactFlow";
+import { NodeData } from "~~/types/commontypes";
+import { notification } from "~~/utils/scaffold-eth";
 
 const ProductPage: NextPage = () => {
   const {
@@ -22,6 +24,16 @@ const ProductPage: NextPage = () => {
     onConnectEnd,
   } = useJourneyForProduct();
   console.log(nodes, edges);
+
+  const frameId = useMemo(() => {
+    const getFirstNodeFrameId = (nodes: any[]) => {
+      const firstNode = nodes.find(node => node.id === "1");
+      console.log("firstNode", firstNode);
+      return firstNode?.data?.frameId;
+    };
+    return getFirstNodeFrameId(nodes);
+  }, [nodes]);
+
   return (
     <div className="flex h-[100vh]">
       <div
@@ -45,7 +57,30 @@ const ProductPage: NextPage = () => {
           onEdgesChange={onEdgesChange}
           onNodeClick={onNodeClick}
         />
+        {frameId && (
+          <button
+            style={{
+              position: "absolute",
+              top: 20,
+              right: 20,
+              zIndex: 999,
+              padding: "10px",
+              background: "white",
+              border: "1px solid black",
+              borderRadius: "5px",
+              cursor: "pointer",
+              color: "black",
+            }}
+            onClick={() => {
+              window.navigator.clipboard.writeText(`https://frames-hojayega.vercel.app/frames/${frameId}`);
+              notification.success("Link copied to clipboard");
+            }}
+          >
+            Link For Warpcast
+          </button>
+        )}
       </div>
+      {/*render button that copies link of current node*/}
 
       <NodeModal
         key={currentNode?.id}
